60ac200a5fbb7e3b6fdc6fdb6353f4aa814802cd
AarhonJoshuaa <aarhonjoshuaa001@gmail.com>
2021-02-15 22:25:55 +0530

    init

---
+++ settings/config.py
@@ -0,0 +1,14 @@
+import os
+ENV = "DEV"  #<--- change here DEV or PROD
+## keys
+if ENV == "DEV":
+ from settings import keys
+ telegram_key = keys.telegram_key
+ mongodb_key = keys.mongodb_key
+elif ENV == "PROD":
+ import ast
+ telegram_key = ast.literal_eval(os.environ["telegram_key"])
+ mongodb_key = ast.literal_eval(os.environ["mongodb_key"])
+## server
+host = "0.0.0.0"
+port = int(os.environ.get("PORT", 5000))
---
+++ settings/keys.py
@@ -0,0 +1,2 @@
+telegram_key='1632392176:AAGl75a13m0Dk-4dRoKzl0q19jWugAf50eI'
+mongodb_key="mongodb+srv://Aarhon:awsedr@cluster0.pzjtm.mongodb.net/Questions?retryWrites=true&w=majority"
